6db519c1d07d716b7c690820a315296b9a57cb1c
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-24 14:41:48 +0430

    add UserRepositoryImpl

---
+++ src/main/java/com/example/bus_online_shopping_system/model/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.bus_online_shopping_system.model.repository.impl;
+
+import com.example.bus_online_shopping_system.model.entity.User;
+import com.example.bus_online_shopping_system.model.repository.UserRepository;
+
+public class UserRepositoryImpl extends RepositoryImpl<User,Integer> implements UserRepository {
+}
