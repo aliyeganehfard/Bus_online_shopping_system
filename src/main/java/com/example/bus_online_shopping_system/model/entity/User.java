0389e32aa119467cc973fc59bb8fae7947fb1ced
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-24 14:38:43 +0430

    add User

---
+++ src/main/java/com/example/bus_online_shopping_system/model/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.bus_online_shopping_system.model.entity;
+
+import com.example.bus_online_shopping_system.model.entity.base.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity
+public class User extends BaseEntity<Integer> {
+        private String username;
+        private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
