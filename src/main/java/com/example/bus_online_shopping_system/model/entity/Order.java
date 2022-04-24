adf4c3cf72d79dde0fd3c784b63f88eef7c57af0
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-24 14:39:24 +0430

    add Order

---
+++ src/main/java/com/example/bus_online_shopping_system/model/entity/Order.java
@@ -0,0 +1,48 @@
+package com.example.bus_online_shopping_system.model.entity;
+
+import com.example.bus_online_shopping_system.model.entity.base.BaseEntity;
+import com.example.bus_online_shopping_system.model.util.Gender;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity
+public class Order extends BaseEntity<Integer> {
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @ManyToOne
+    private Ticket ticket;
+
+    @ManyToOne
+    private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Order order = (Order) o;
+        return getId() != null && Objects.equals(getId(), order.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
