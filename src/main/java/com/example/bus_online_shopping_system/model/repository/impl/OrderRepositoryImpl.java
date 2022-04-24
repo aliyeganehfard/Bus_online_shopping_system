bd34d181c569694360816e59d36ed63e3feb5f34
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-24 14:42:46 +0430

    add OrderRepositoryImpl

---
+++ src/main/java/com/example/bus_online_shopping_system/model/repository/impl/OrderRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.bus_online_shopping_system.model.repository.impl;
+
+import com.example.bus_online_shopping_system.model.entity.Order;
+import com.example.bus_online_shopping_system.model.repository.OrderRepository;
+
+public class OrderRepositoryImpl extends RepositoryImpl<Order,Integer> implements OrderRepository {
+}
