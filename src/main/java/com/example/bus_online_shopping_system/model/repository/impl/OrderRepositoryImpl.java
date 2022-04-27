b5b7681977c5cdba2cffbdbb66466a2dad04d8c9
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-27 21:44:20 +0430

    fix add OrderRepositoryImpl

--- src/main/java/com/example/bus_online_shopping_system/model/repository/impl/OrderRepositoryImpl.java
+++ src/main/java/com/example/bus_online_shopping_system/model/repository/impl/OrderRepositoryImpl.java
@@ -3,5 +3,21 @@ package com.example.bus_online_shopping_system.model.repository.impl;
 import com.example.bus_online_shopping_system.model.entity.Order;
 import com.example.bus_online_shopping_system.model.repository.OrderRepository;
 
+import java.util.List;
+
 public class OrderRepositoryImpl extends RepositoryImpl<Order,Integer> implements OrderRepository {
+    @Override
+    public List<Order> getUserOrder(Integer userId) {
+        try(var session = getSessionFactory().openSession()){
+            try {
+                return session.createQuery("SELECT o FROM Order o " +
+                        "WHERE o.user.id = :userId",Order.class)
+                        .setParameter("userId",userId)
+                        .list();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 }
