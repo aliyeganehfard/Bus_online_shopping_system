00bd5e6479e17b80e13e85da3579ddf3cf602134
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-27 21:43:56 +0430

    fix add OrderRepository

--- src/main/java/com/example/bus_online_shopping_system/model/repository/OrderRepository.java
+++ src/main/java/com/example/bus_online_shopping_system/model/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.example.bus_online_shopping_system.model.repository;
 
+import com.example.bus_online_shopping_system.model.entity.Order;
+
+import java.util.List;
+
 public interface OrderRepository {
+    List<Order> getUserOrder(Integer userId);
 }
