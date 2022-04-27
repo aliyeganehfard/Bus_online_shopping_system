c903835c4c0eead9e6aa87a5864b70a1497cfdad
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-04-27 21:45:05 +0430

    fix add OrderServiceImpl

--- src/main/java/com/example/bus_online_shopping_system/controller/impl/OrderServiceImpl.java
+++ src/main/java/com/example/bus_online_shopping_system/controller/impl/OrderServiceImpl.java
@@ -4,10 +4,17 @@ import com.example.bus_online_shopping_system.controller.OrderService;
 import com.example.bus_online_shopping_system.model.entity.Order;
 import com.example.bus_online_shopping_system.model.repository.impl.OrderRepositoryImpl;
 
+import java.util.List;
+
 public class OrderServiceImpl extends ServiceImpl<OrderRepositoryImpl, Order,Integer> implements OrderService {
     private OrderRepositoryImpl orderRepository;
     public OrderServiceImpl() {
         super(new OrderRepositoryImpl());
         this.orderRepository=new OrderRepositoryImpl();
+    }
+
+    @Override
+    public List<Order> getUserOrder(Integer userId) {
+        return orderRepository.getUserOrder(userId);
     }
 }
